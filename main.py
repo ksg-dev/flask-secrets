8f448d602ab7d3d097ede3e1dae975fdda141e5d
ksg-dev <ksg.dev.data@gmail.com>
2024-07-26 09:59:33 -0500

    add login route

--- main.py
+++ main.py
@@ -22,5 +22,10 @@ def home():
     return render_template('index.html')
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
 if __name__ == '__main__':
     app.run(debug=True)
